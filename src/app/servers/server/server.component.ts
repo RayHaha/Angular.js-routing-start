21082326074e301fc51fc9f7b8159cc157d82586
RayHaha <g820324@gmail.com>
2019-09-25 21:31:31 -0400

    server component

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -12,7 +12,7 @@ export class ServerComponent implements OnInit {
   server: { id: number, name: string, status: string };
 
   constructor(private serversService: ServersService,
-              private route: ActivatedRoute) { }
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
     const id = +this.route.snapshot.params['id'];
