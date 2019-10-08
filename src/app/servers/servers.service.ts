8e8f6137f13f59a9681f3e777832f04358bd905f
RayHaha <g820324@gmail.com>
2019-10-08 00:34:26 -0400

    servers service

--- src/app/servers/servers.service.ts
+++ src/app/servers/servers.service.ts
@@ -1,4 +1,5 @@
 export class ServersService {
+  
   private servers = [
     {
       id: 1,
@@ -20,7 +21,7 @@ export class ServersService {
   getServers() {
 
     return this.servers;
-    
+
   }
 
   getServer(id: number) {
