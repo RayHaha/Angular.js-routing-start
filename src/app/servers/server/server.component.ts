6dc27f480fe2d7b5925edb5cd614fbceb993ca8f
RayHaha <g820324@gmail.com>
2019-10-27 00:28:10 -0400

    server component

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -9,6 +9,7 @@ import { ServersService } from '../servers.service';
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.css']
 })
+
 export class ServerComponent implements OnInit {
   
   server: { id: number, name: string, status: string };
