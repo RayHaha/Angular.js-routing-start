ab91a63b636377bf62f46a405f9b8eb9841e8f70
RayHaha <g820324@gmail.com>
2019-11-25 20:18:02 -0500

    auth service ts

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -3,6 +3,7 @@ export class AuthService{
     loggedIn = false;
 
     isAuthenticated(){
+        
         const promise = new Promise(
             (resolve, reject) => {
                 setTimeout(
